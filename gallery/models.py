8bc1d1b3b2a1dc4b62a65528495eb8dc3dbb8029
billowbashir <billowbashir@gmail.com>
2018-09-28 18:26:45 +0300

    deisplay images

--- gallery/models.py
+++ gallery/models.py
@@ -1,26 +1,33 @@
 from django.db import models
 
-# Image
-# Image Name.
-# Image Description.
-# Image Location Foreign Key.
-# Image category Foreign Key.
-
 class Location(models.Model):
     name=models.CharField(max_length=30)
 
     def save_location(self):
         self.save()
 
+    def _str__(self):
+        return self.name
 class Category(models.Model):
     name=models.CharField(max_length=30)
 
     def save_category(self):
         self.save()
 
+    def _str__(self):
+        return self.name
+
 class Image(models.Model):
     image=models.ImageField(upload_to='photos/')
     image_name=models.CharField(max_length=30)
     image_description=models.CharField(max_length=60)
-    location=models.ForeignKey(Location)
-    category=models.ForeignKey(Category)
+    location=models.ForeignKey('Location')
+    category=models.ForeignKey('Category')
+
+    def _str__(self):
+        return self.name
+    # @classmethod
+    # def get_all(cls):
+    #
+    #  images = cls.objects.all()
+    #  return images
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image,Location,Category
 
 def welcome(request):
-    return render(request,'welcome.html')
+    images=Image.objects.all()
+    return render(request,'welcome.html',{"images":images})
