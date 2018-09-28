24ccb0c059d23472f3ec253cdfe81ad140d9a0e5
billowbashir <billowbashir@gmail.com>
2018-09-28 14:34:12 +0300

    add navbar to home page

--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome(request):
-    return HttpResponse('welcome to my gallery app')
+    return render(request,'welcome.html')
