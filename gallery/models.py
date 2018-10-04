1b2488c3e64cfaae2b80cdbae29fbdee4cab4ab8
billowbashir <billowbashir@gmail.com>
2018-10-04 12:05:47 +0300

    add methods to models and test them

--- gallery/models.py
+++ gallery/models.py
@@ -6,6 +6,12 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
+    def delete_locations(self):
+        self.delete()
+
+    def update_locations(self):
+        self.update( )
+
     def _str__(self):
         return self.name
 class Category(models.Model):
@@ -13,6 +19,12 @@ class Category(models.Model):
 
     def save_category(self):
         self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update()
 
     def _str__(self):
         return self.name
@@ -24,15 +36,28 @@ class Image(models.Model):
     location=models.ForeignKey('Location')
     category=models.ForeignKey('Category')
 
-    def _str__(self):
-        return self.name
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update( )
 
     @classmethod
     def search_by_category(cls,search_term):
         images=cls.objects.filter(category__name__icontains=search_term)
         return images
-    # @classmethod
-    # def get_all(cls):
-    #
-    #  images = cls.objects.all()
-    #  return images
+    @classmethod
+    def filter_by_location(cls , location):
+        locations=cls.objects.filter( location=location )
+        return locations
+
+    @classmethod
+    def get_image_by_id(cls):
+        Image.cls.objects.filter( id )
+        return Image
--- gallery/tests.py
+++ gallery/tests.py
@@ -4,6 +4,9 @@ from .models import Image,Location,Category
 class LocationTestClass(TestCase):
     def setUp(self):
         self.new_location=Location(name='Ngong')
+    def tearDown(self):
+        Location.objects.all().delete()
+
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_location,Location))
@@ -11,14 +14,53 @@ class LocationTestClass(TestCase):
         self.new_location.save_location()
         location=Location.objects.all()
         self.assertTrue(len(location)>0)
+    def test_delete_location(self):
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        location=Location.objects.all()
+        self.assertEqual(len(location),0)
+    def test_update_location(self):
+        self.new_location.save_location()
+        self.new_location.update_locations(name='Kenya')
+        self.assertEqual(self.new_location.name,'Kenya')
 
 class CategoryTestClass(TestCase):
     def setUp(self):
         self.new_category=Category(name='Nature')
-
+    def tearDown(self):
+        Category.objects.all().delete()
     def test_instance(self):
         self.assertTrue(isinstance(self.new_category,Category))
     def test_save_category(self):
         self.new_category.save_category()
         category=Category.objects.all()
         self.assertTrue(len(category)>0)
+    def test_delete_category(self):
+        self.new_category.save_category()
+        self.new_category.delete_category()
+        category=Category.objects.all()
+        self.assertEqual(len(category),0)
+    def test_update_category(self):
+        self.new_category.save_category()
+        self.new_category.update_category(name='sports')
+        self.assertEqual(self.new_category.name,'sports')
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.new_image=Image(name='hazard')
+    def tearDown(self):
+        Image.objects.all().delete()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+    def test_save_image(self):
+        self.new_image.save_image()
+        image=Image.objects.all()
+        self.assertTrue(len(image)>0)
+    def test_delete_image(self):
+        self.new_image.save_image()
+        self.new_image.delete_image()
+        image=Image.objects.all()
+        self.assertEqual(len(image),0)
+    def test_update_image(self):
+        self.new_image.save_image()
+        self.new_image.update_image(name='flask')
+        self.assertEqual(self.new_image.name,'flask')
